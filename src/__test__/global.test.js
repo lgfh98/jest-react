445f20491a4714e732a1bd13e791186544524e9e
lgfh98 <lgfigueroah@outlook.com>
2021-10-11 08:33:21 -0500

    mas pruebas

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,28 @@
 const text = "Hola Mundo";
+const fruits = ["🍎", "🍏", "🍇", "🍉", "🍌", "🍊"];
 
 test("Debe contener un texto", () => {
   expect(text).toMatch(/Mundo/);
+});
+
+test("¿Tenemos banana?", () => {
+  expect(fruits).toContain("🍌");
+});
+
+test("Mayor que", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test("Es verdadero?", () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, cb) => {
+  cb(str.split("").reverse().join(""));
+};
+
+test("Probando Callback", () => {
+  reverseString("Hola", (str) => {
+    expect(str).toBe("aloH");
+  });
 });
